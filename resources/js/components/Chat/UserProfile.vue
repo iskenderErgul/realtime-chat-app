fb67e90d049e1595c276838972cca30aa57f0cf1
İskender <100355107+iskenderErgul@users.noreply.github.com>
2024-07-11 11:33:56 +0300

    .

---
+++ resources/js/components/Chat/UserProfile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>User Profile</h2>
+        <p>User ID: {{ $route.params.userId }}</p>
+
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const userId = router.currentRoute.value.params.userId;
+
+
+</script>
